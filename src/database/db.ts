faf02e852540b72d3129dbf8ec37d700b8e27592
chrod <chrod.dev@gmail.com>
2023-07-25 10:37:28 -0500

    Remove ssl from pool

--- src/database/db.ts
+++ src/database/db.ts
@@ -2,8 +2,7 @@ import { Pool } from 'pg';
 import config from '../config/config';
 
 const pool = new Pool({
-  connectionString: config.DATABASE_URL,
-  ssl: false
+  connectionString: config.DATABASE_URL
 });
 
 export default pool;
